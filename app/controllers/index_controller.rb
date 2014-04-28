18e50c058ea4ba331f8c379646e33d9cfe052f21
anastasia143 <semenova.anastasia143@yandex.ru>
2014-04-28 19:21:34 +0400

    new table structure

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,6 @@
 class IndexController < ApplicationController
-
+        helper_method :taskCountInHometask
+        helper_method :hometaskCount
 	def home
 	  @users = User.all
        	  @tasks = Task.all
@@ -17,6 +18,22 @@ class IndexController < ApplicationController
 	end
 
         def managment
+
+        end
+
+        def self.taskCountInHometask(searched_homework)
+           return Task.where(homework: searched_homework).count
+        end
+
+        def self.hometaskCount
+           @tasks2 = Task.all
+           array = [2]
+           @tasks2.each do |t|
+               if !array.include?(t.homework)
+                   array.push(t.homework)
+               end
+           end
+           return array.length
         end
 
 end
