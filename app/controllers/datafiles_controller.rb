96975e6921d42b307cbf4b960c66fb6e6b3150c8
anastasia143 <semenova.anastasia143@yandex.ru>
2014-05-01 10:23:41 +0400

    directory in upload method

--- app/controllers/datafiles_controller.rb
+++ app/controllers/datafiles_controller.rb
@@ -17,7 +17,7 @@ class DatafilesController < ApplicationController
 
    def upload
       uploaded_io = params[:datafile][:dfile]
-      File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
+      File.open(Rails.root.join('public', 'uploads', current_user.email, uploaded_io.original_filename), 'wb') do |file|
       		file.write(uploaded_io.read)
   	 end
    end
