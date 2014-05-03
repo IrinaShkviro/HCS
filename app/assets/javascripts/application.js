3ff24ca3b439462b492efc8a50dd49728e9137f4
IrinaShkviro <IrinaShkviro13@gmail.com>
2014-05-03 16:40:23 +0400

    Fix problem with dropdown menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,5 @@
 // This is a manifest file that'll be compiled into application.js, which will include all the files
+
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
@@ -9,8 +10,13 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+
+
 //= require jquery
+
 //= require jquery_ujs
+
+//= require bootstrap
 //= require turbolinks
+
 //= require_tree .
